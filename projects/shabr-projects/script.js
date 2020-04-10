d74bbbf55a18e4a63918cd9b095360a9fbd94ab2
stephenie lin <stepheniel7340@hstat.org>
2020-04-10 02:47:18 +0000

    make shabr portfolio

---
+++ projects/shabr-projects/script.js
@@ -0,0 +1,9 @@
+let ownerName = "Stephenie Lin"
+let userName = "stepheniel7340";
+
+document.querySelectorAll(".owner-name").forEach((e) => {
+    e.innerHTML = ownerName;
+});
+
+document.querySelector("#github").href = "http://github.com/" + userName;
+document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
