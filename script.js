b0c85372d1c75419201072d82a371975db3767e4
stephenie lin <stepheniel7340@hstat.org>
2020-04-12 03:28:31 +0000

    fix index

--- script.js
+++
@@ -1,9 +0,0 @@
-let ownerName = "Stephenie Lin"
-let userName = "stepheniel7340";
-
-document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
-});
-
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
